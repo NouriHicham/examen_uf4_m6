a3e6d8427c07cda9fb63f87824e1442c03ded0cb
NouriHicham <nourichamidhicham@fpllefia.com>
2025-06-03 18:06:49 +0200

    arreglar image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function Header() {
   return (
     <header className="flex justify-between items-center p-4">
       <div className="flex items-center">
         <Link href="/">
-          <img src="/vercel.svg" alt="logo" className="w-10" />
+          <Image src="/vercel.svg" alt="logo" width={20} height={20}/>
         </Link>
         <h1 className="ml-2">Examen UF4 M6 - Peliculas</h1>
       </div>
