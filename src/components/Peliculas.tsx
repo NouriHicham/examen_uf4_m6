6444a72aab4292b83bf8dff6abe86cdf64ff3f55
NouriHicham <nourichamidhicham@fpllefia.com>
2025-06-03 18:36:58 +0200

    arreglar despliegue

--- src/components/Peliculas.tsx
+++ src/components/Peliculas.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function getPopularMovies() {
   const res = await fetch('https://api.themoviedb.org/3/movie/popular', {
     method: 'GET',
@@ -18,10 +20,19 @@ export default async function Peliculas() {
   return (
     <div className="max-w-7xl mx-auto px-4 py-12">
       <h1 className="text-3xl font-bold mb-4">Popular Movies</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <form
+        className="mb-6 flex items-center gap-2"
+      >
+        <input
+          type="text"
+          placeholder="Buscar película..."
+          className="px-3 py-2 rounded border border-gray-300 focus:outline-none focus:ring w-full max-w-xs"
+        />
+      </form>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
         {pelis.results.map((peli: any) => (
           <div key={peli.id} className=" rounded-lg shadow-md p-4">
-            <img src={`https://image.tmdb.org/t/p/w500/${peli.poster_path}`} alt={peli.title} className="w-full object-cover" />
+            <Image src={`https://image.tmdb.org/t/p/w500/${peli.poster_path}`} alt={peli.title} className="w-full object-cover" width={500} height={500}/>
             <div className="pt-4">
               <h2 className="text-lg font-bold mb-1 text-white">{peli.title}</h2>
               <p className="text-white mb-2">
